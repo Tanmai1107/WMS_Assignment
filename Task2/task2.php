c5bf21c6d151f90544f3e58ab760d155ad6bd81e
Tanmai1107 <69008859+Tanmai1107@users.noreply.github.com>
2021-09-13 22:20:10 +0530

    Update task2.php

--- Task2/task2.php
+++ Task2/task2.php
@@ -1,19 +1,8 @@
 <html>
     <body>
-    <script>
-    function myFunction() {
-        var x;
-        var r = confirm("Are You Sure?");
-        if (r == true) {
-            x = "You pressed OK!";
-    }
-        else {
-            x = "You pressed Cancel!";
-    }
-        document.getElementById("demo").innerHTML = x;
-}
-</script>
         <?php
             
-            echo "<a onclick='myFunction()' href='comment-delete?id=1' class='red-text'>Delete</a>";
+            echo '<a href="comment-delete?id=1" onclick="return confirm(\'Are You Sure?\');">Delete</a>';
         ?>
+    </body>
+</html>
